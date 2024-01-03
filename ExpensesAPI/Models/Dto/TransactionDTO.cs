006f5b025d17110009bca7af31dc1d4dab12d336
Luan Manara <manara_luan@hotmail.com>
2024-01-03 17:42:40 -0300

    transactionName on DTO

--- ExpensesAPI/Models/Dto/TransactionDTO.cs
+++ ExpensesAPI/Models/Dto/TransactionDTO.cs
@@ -5,10 +5,32 @@ namespace ExpensesAPI.Models
 {
     public class TransactionDTO
     {
+        private int _transactionType;
         public int Id { get; set; }
         public int PeriodId { get; set; }
         public double Value { get; set; }
-        public int TransactionType { get; set; }
+        public int TransactionType
+        {
+            get { return _transactionType; }
+            set
+            {
+                _transactionType = value;
+
+                if (TransactionType == 1) 
+                {
+                    TransactionName = "Crédito";
+                }
+                else if (TransactionType == 2)
+                {
+                    TransactionName = "Débito";
+                }
+                else
+                {
+                    TransactionName = "Salário";
+                }
+            }
+        }
+        public string TransactionName { get; set; }
         public string Description { get; set; }
         public DateTime DateOfMovement { get; set; }
     }
